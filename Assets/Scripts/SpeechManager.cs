d3a64b0648f22af41ecc518c75e6539c6edeea30
Yoshiaki Nishimura <yoshiaki@isurf.sg>
2017-06-05 10:14:16 +0800

    initial commit

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+
+    public Text CortanaText;
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Reset", () =>
+        {
+            // Call the OnReset method on every descendant object.
+            this.BroadcastMessage("OnReset");
+        });
+
+        keywords.Add("Right", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Right");
+            }
+        });
+        keywords.Add("Left", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Left");
+            }
+        });
+        keywords.Add("Front", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Front");
+            }
+        });
+        keywords.Add("Behind", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Behind");
+            }
+        });
+        keywords.Add("Bigger", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Bigger");
+            }
+        });
+        keywords.Add("Grow", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Grow");
+            }
+        });
+
+        keywords.Add("Shrink", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("Shrink");
+            }
+        });
+
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+
+        CortanaText.text = "";
+
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+            CortanaText.text = args.text;
+        }
+    }
+}
---
+++ Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    public float RotationSensitivity = 10.0f;
+    private float rotationFactor;
+    Vector3 originalPosition;
+    Vector3 originalScale;
+    Vector3 NewPosition;
+    Vector3 tempPosition;
+    float yRotation = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the sphere when the app starts.
+        originalPosition = this.transform.localPosition;
+        originalScale = this.transform.localScale;
+
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+
+
+    // Called by SpeechManager when the user says the "Reset world" command
+    void OnReset()
+    {
+        // If the sphere has a Rigidbody component, remove it to disable physics.
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            DestroyImmediate(rigidbody);
+        }
+
+        // Put the sphere back into its original local position.
+        this.transform.localPosition = originalPosition;
+        this.transform.localScale = originalScale;
+    }
+
+
+    void Right()
+    {
+      
+        yRotation = 90.0f;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+    }
+    void Behind()
+    {
+        
+        yRotation = 180.0f;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+    }
+    void Left()
+    {
+      
+        yRotation = 270.0f;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+    }
+    void Front()
+    {
+     
+        yRotation = 0.0f;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+    }
+
+    void Grow()
+    {
+        transform.localScale = new Vector3(2, 2, 2);
+        //transform.localPosition = new Vector3(0, -10, 80);
+       
+    }
+    void Bigger()
+    {
+        transform.localScale = new Vector3(5, 5, 5);
+        //transform.localPosition = new Vector3(0, -10, 80);
+
+    }
+    void Shrink()
+    {
+        //float yRotation = 90.0f;
+        transform.localScale = originalScale;
+        transform.localPosition = originalPosition;
+    }
+}
