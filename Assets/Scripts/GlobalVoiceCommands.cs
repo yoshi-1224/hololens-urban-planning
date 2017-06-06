4a0696533ccb1df02e7cb5962511c76e2ecb985f
yoshi-1224 <yoshiaki.bb@gmail.com>
2017-06-06 17:30:25 +0800

    Import ION prefab

---
+++ Assets/Scripts/GlobalVoiceCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class GlobalVoiceCommands : MonoBehaviour {
+
+    void Start () {
+        if (InputManager.Instance == null) {
+            return;
+        }
+        InputManager.Instance.AddGlobalListener(gameObject);
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnDestroy() {
+        if (InputManager.Instance == null) {
+            return;
+        }
+        InputManager.Instance.RemoveGlobalListener(gameObject);
+    }
+
+    public void changeMapLayout(string layoutType) {
+        switch (layoutType) {
+            case "":
+
+            default:
+                break;
+        }
+    }
+}
